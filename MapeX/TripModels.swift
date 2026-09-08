5c20c06ff149f7925f8607b053ce312875f7f7a0
Mirac Kutay <MiracKutayMac@icloud.com>
2026-09-08 23:08:38 +0300

    Telif başlıkları

--- MapeX/TripModels.swift
+++ MapeX/TripModels.swift
@@ -1,3 +1,4 @@
+// Copyright (c) 2026 Mirac Kutay Sereflisan. Tum haklari saklidir.
 import SwiftUI
 
 // MARK: - Sürüş Modları
--- Tools/kgm_tarife_scraper.py
+++ Tools/kgm_tarife_scraper.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# Copyright (c) 2026 Mirac Kutay Sereflisan. Tum haklari saklidir.
 """
 KGM / Kuzey Marmara otoyol tarife PDF'lerini uygulamanın okuduğu JSON'a çevirir.
 
